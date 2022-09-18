a79ab944f2cf027928e076e4d45f143f3f4b4aca
Nohealforu <113795898+Nohealforu@users.noreply.github.com>
2022-09-18 00:05:36 -0400

    Update mapping.js

--- FF1NESMappingTool/mapping.js
+++ FF1NESMappingTool/mapping.js
@@ -310,8 +310,8 @@ var overworldMap = {
     cells: overworldCells,
     data: null,
     getTile: function (mapCell, col, row) {
-        console.log("mapCell: " + mapCell + ". col: " + col + ". row: " + row + ". calc: " + mapCell * overworldCells.rows * overworldCells.cols + row * dungeonCells.cols + col);
-        this.overworldTileIndex[mapCell * overworldCells.rows * overworldCells.cols + row * dungeonCells.cols + col];
+        console.log("mapCell: " + mapCell + ". col: " + col + ". row: " + row + ". calc: " + (mapCell * overworldCells.rows * overworldCells.cols + row * dungeonCells.cols + col));
+        this.overworldTileIndex[mapCell * overworldCells.rows * overworldCells.cols + row * overworldCells.cols + col];
     }
 };
 
