3011d6ef451049e4a612b45a57851f1fbfc0cb80
Nohealforu <113795898+Nohealforu@users.noreply.github.com>
2022-09-22 02:32:08 -0400

    Update mapping.js

--- FF1NESMappingTool/mapping.js
+++ FF1NESMappingTool/mapping.js
@@ -2158,7 +2158,7 @@ Controller.prototype.touchHandler = function(e)
 		let rect = this.canvas.getBoundingClientRect();
 		let offsetLeft = rect.left + window.scrollX;
 		let offsetTop = rect.top + window.scrollY;
-		document.getElementById('touchTest').innerHTML = ('pageX: ' + e.touches[0].pageX + ', pageY: ' + e.touches[0].pageY + ', offsetX: ' + offsetLeft + ', offsetY: ' + offsetTop;
+		document.getElementById('touchTest').innerHTML = ('pageX: ' + e.touches[0].pageX + ', pageY: ' + e.touches[0].pageY + ', offsetX: ' + offsetLeft + ', offsetY: ' + offsetTop);
 		for(let i = 0; i < e.touches.length; i++)
 		{
 			let touch = e.touches[i];
